6d537b98960d2c823fe5241c8e37fa7ca1d823f8
Marce1193 <marceloacuna11@gmail.com>
2023-10-09 23:04:42 -0300

    Manejo de archivos

--- ProducManager.js
+++ ProducManager.js
@@ -1,45 +1,70 @@
+const fs = require("fs")
+
 class ProductManager {
   constructor() {
-    this.products = []
+    this.path = "./files/Products.json"
   }
 
-  addProduct = (title, description, price, thumbnail, code, stock) => {
-    const product = {
-      title,
-      description,
-      price,
-      thumbnail,
-      code,
-      stock: stock ?? 1,
-      id: this.products.length + 1,
-    }
-
-    if (this.products.some((product) => product.code === code)) {
-      console.log(`El código ${product.code}ya existe`)
-      return
-    }
-
-    if (!title || !description || !price || !thumbnail || !code || !stock) {
-      console.log("todos los campos son requeridos")
-      return
-    }
-
-    this.products.push(product)
-  }
-
-  getProducts = () => {
-    console.log(this.products)
-    return
-  }
-
-  getProductById = (productId) => {
-    let prod = this.products.find((prod) => prod.id === productId)
-    if (prod) {
-      console.log(prod)
+  getProducts = async () => {
+    if (fs.existsSync(this.path)) {
+      const productsString = await fs.promises.readFile(this.path, "utf-8")
+      const products = JSON.parse(productsString)
+      return products
     } else {
-      console.log("not found")
+      return []
     }
+  }
+
+  addProduct = async (product) => {
+    const products = await this.getProducts()
+
+    product.id =
+      products.length === 0 ? 1 : products[products.length - 1].id + 1
+
+    products.push(product)
+
+    await fs.promises.writeFile(this.path, JSON.stringify(products, null, "\t"))
+    return product
+  }
+
+  getProductById = async (id) => {
+    const products = await this.getProducts()
+    const product = products.find((product) => product.id === id)
+    if (!product) return `Error: Product with id: ${id} does not exist.`
+    return product
+  }
+
+  updateProduct = async (id, changes) => {
+    const products = await this.getProducts()
+    const product = await this.getProductById(id)
+    const productIndex = products.findIndex((product) => product.id === id)
+
+    if (changes.id) {
+      return "Error: Cannot modify id property"
+    }
+
+    const updatedProduct = {
+      ...product,
+      ...changes,
+    }
+
+    products.splice(productIndex, 1, updatedProduct)
+
+    await fs.promises.writeFile(this.path, JSON.stringify(products, null, "\t"))
+    return updatedProduct
+  }
+
+  deleteProduct = async (id) => {
+    const products = await this.getProducts()
+    const productIndex = products.findIndex((product) => product.id === id)
+
+    if (productIndex === -1)
+      return `Error: Product with id ${id} does not exist.`
+
+    products.splice(productIndex, 1)
+    await fs.promises.writeFile(this.path, JSON.stringify(products, null, "\t"))
+    return "Product deletion: successful!"
   }
 }
 
-const productManager = new ProductManager()
+module.exports = ProductManager
